c35d5071c4619bd4ab0260924b214b9ba2a5fdf8
Pavlina <pavlina122134@gmail.com>
2025-05-21 09:46:39 +0300

    Add files via upload
    Signed-off-by: Pavlina <pavlina122134@gmail.com>

---
+++ projekt.1/js/slider.js
@@ -0,0 +1,54 @@
+'use strict'
+let currentIndex = 0;
+const slider = document.querySelectorAll(".attractions__item");
+const prevButton = document.querySelector(".attractions__left");
+const nextButton = document.querySelector(".attractions__right");
+const visibleCards = 3;
+updateSlider();
+prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = slider.length - visibleCards;
+    }
+    updateSlider();
+});
+nextButton.addEventListener("click", () => {
+    if (currentIndex < slider.length - visibleCards) {
+        currentIndex++;
+    }
+    else {
+        currentIndex = 0;
+    }
+    updateSlider();
+});
+function updateSlider() {
+    slider.forEach((item, index) => {
+        if (index >= currentIndex && index < currentIndex + visibleCards) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
+const EraContainer = document.querySelector(".attractions");
+
+if (EraContainer) {
+    console.log('достопримечательности существуют');
+    const dataTitleEra = [
+        "Волжск",
+        "5000 до н.э- 500 н.э",
+        "500- 1500",
+        "14 век- 1590-е годы",
+        "1500-1914",
+        "1914- настоящее",
+    ];
+
+    const titleEra =
+            EraContainer.querySelectorAll(".attractions__data");
+
+            titleEra.forEach((item, index) => {
+                item.textContent = dataTitleEra[index];
+            });
+     } 
